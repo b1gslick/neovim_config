73463001e057141a967ee8a7378a34f82383acc6
s_timokhin <s_timokhin@wargaming.net>
2023-01-15 13:48:23 +0200

    first iter

---
+++ cmd.md
@@ -0,0 +1,2 @@
+gcc -- comment one line
+gc{number of line}j comment numbers of line  
---
+++ lua/timon/core/options.lua
@@ -0,0 +1,53 @@
+local opt = vim.opt -- for conciseness
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tab & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+
+opt.smartcase = true
+
+-- cursor line
+opt.cursorline = true
+
+-- appearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
+
+opt.iskeyword:append("-")
+
+-- Treesitter folding
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+-- Vim inspector
+
+vim.cmd([[
+let g:vimspector_sidebar_width = 85
+let g:vimspector_bottombar_height = 15
+let g:vimspector_terminal_maxwidth = 70
+]])
+
+vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]])
